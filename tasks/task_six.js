09a6bb2ab634632f9251b92189d17f66180b0d30
Chidubem <nwekedubemph@gmail.com>
2023-07-06 07:11:50 -0700

    second commit

--- tasks/task_six.js
+++ tasks/task_six.js
@@ -1,2 +1,28 @@
+//Missing parentheses
+if (x === 5) {
+  console.log("x is equal to 5");
+}
+
+
+//Missing semicolon
 const message = "Hello, world";
-console.log(message);
+console.log(message)
+
+
+//Incorrect closing brace placement
+function sayHello() {
+    console.log("Hello!");
+    console.log("Goodbye!");
+}
+
+
+//Mismatched opening and closing brackets
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers[2]);
+
+
+//Incorrect function definition
+const addNumbers = function(x, y) {
+  return x + y;
+};
+console.log(addNumbers(5, 10));
