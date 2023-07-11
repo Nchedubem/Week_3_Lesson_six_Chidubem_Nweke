5d2852f277f1083cccc071fd7863635b495d983e
Chidubem <nwekedubemph@gmail.com>
2023-07-11 06:21:03 -0700

    second commit

--- tasks/task_five.js
+++ tasks/task_five.js
@@ -1,10 +1,10 @@
 // Create multiple objects with different properties
-var obj1 = { name: "Chidubem Orakwelu" };
-var obj2 = { age: 29 };
-var obj3 = { occupation: "Software Engineer" };
+let object1 = { name: "Chidubem Orakwelu" };
+let object2 = { age: 29 };
+let object3 = { occupation: "Software Engineer" };
 
 // Merge the objects into a single object using Object.assign()
-var mergedObject = Object.assign({}, obj1, obj2, obj3);
+let mergedObject = Object.assign({}, object1, object2, object3);
 
 // Log the merged object to the console
 console.log(mergedObject);
